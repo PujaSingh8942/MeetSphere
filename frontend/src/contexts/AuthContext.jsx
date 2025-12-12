554740b25520aa0d996d11eef8dfec76dccbd1ec
Puja Singh <pujasingh8942@gmail.com>
2025-12-12 18:10:35 +0530

    Modified AuthContext

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import server from "../enviroment";
+import server from "../environment";
 
 console.log("API URL:", process.env.REACT_APP_API_URL);
 
